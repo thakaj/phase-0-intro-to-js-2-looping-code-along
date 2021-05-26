d1b882c123ab0d548dfec82a691a8babeb7aab69
thakaj <taulant.hakaj@scranton.edu>
2021-05-26 19:58:44 -0400

    commit tree d6343d70a079ab4f009cceaaf07a2c8d87ed064b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names){
+    const party = []
+    for (let i=0; i < names.length; i++){
+        party.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return party;
+}
+
+function countDown(number){
+    for (let i=number; i >= 0; i--){
+        console.log(i);
+    }
+}
